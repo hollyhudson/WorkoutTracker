7e75d8b40010b6aba1b27470049a6c7db4f4218f
Holly Hudson <holly@local>
2018-09-25 18:13:21 +0200

    initial commit, reads a file, splits on newline, and writes to console

--- WorkoutTracker/ViewController.swift
+++ WorkoutTracker/ViewController.swift
@@ -9,12 +9,26 @@
 import UIKit
 
 class ViewController: UIViewController {
+	
+	var contents = String()
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
+		let routineFile = URL(fileURLWithPath: "/Users/holly/Library/Mobile Documents/com~apple~CloudDocs/WorkoutTracker/routine.txt")
+		
+		func processRoutineFile(routine: URL) {
+			do {
+				contents = try String(contentsOf: routine)
+			} catch {
+				print("couldn't read file")
+			}
+			let exercises = contents.split(separator: "\n")
+			for exercise in exercises {
+				print("\(exercise)")
+			}
+		}
+		processRoutineFile(routine: routineFile)
 	}
-
-
 }
 
